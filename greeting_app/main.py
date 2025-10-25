1ceba6570eeded846e550412dc2f38677a66cfe8
Ayan Arfeen <ayanarfeenofficial@gmail.com>
2025-10-26 02:05:18 +0500

    Improved code's logic and some minor changes

--- greeting_app/main.py
+++ greeting_app/main.py
@@ -2,7 +2,16 @@
 
 print("Welcome to our Program!!!")
 # Take name input
-name = str(input("What your's name?: ")).strip().title()
+name = str(input("What's your name?: ")).strip().title()
 
-# Print message
-print(f"\nHi, {name}!!\n")
+if not name: #Checks empty variable
+    print("Enter your name please.")
+
+elif any(ch.isdigit() for ch in name): # Check numbers in name
+    print("Names don't contain numbers!")
+
+else:
+    # Print message
+    print("-" * 30)
+    print(f"Hi, {name}!!")
+    print("-" * 30)
