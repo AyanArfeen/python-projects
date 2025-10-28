087ac876fdfb3c113f99e98786ba41787b2bc5a8
Ayan Arfeen <ayanarfeenofficial@gmail.com>
2025-10-29 00:25:03 +0500

    feat: add area calculator for circle, rectangle, and triangle

---
+++ area_calculator/area_finder.py
@@ -0,0 +1,47 @@
+# This program calculate area of given shapes
+import math
+
+while True:
+    shape = input("Which shape's area do you want to find?\n1. Circle\n2. Rectangle\n3. Triangle\nOr (type 'q' to quit): ")
+    if shape.lower() == 'q':
+        break
+
+    if shape.lower() in ["1", "circle"]:
+        try:
+            radius = float(input("Enter the Radius: "))
+
+        except ValueError:
+            print("Please enter correct values!\n")
+            continue
+        
+        calculations = math.pi * (radius ** 2)
+
+    elif shape.lower() in ["2", "rectangle"]:
+        try:
+            length = float(input("Enter the Length: "))
+            width = float(input("Enter the width: "))
+        
+        except ValueError:
+            print("Please enter correct values!\n")
+            continue
+
+        calculations = length * width
+
+    elif shape.lower() in ["3", "triangle"]:
+        try:
+            base = float(input("Enter the Base: "))
+            height = float(input("Enter the Height: "))
+        
+        except ValueError:
+            print("Please enter correct values!\n")
+            continue
+        
+        calculations = 0.5 * base * height
+
+    else:
+        print("Please select a correct number!\n")
+        continue
+
+    print("-" * 40)
+    print(f"The Area is: {calculations:.2f}")
+    print("-" * 40)
