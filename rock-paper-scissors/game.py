302a7e6bca3f280f0028bcda5308961a6f153119
Ayan Arfeen <ayanarfeenofficial@gmail.com>
2025-11-26 02:10:11 +0500

    feat: add basic rock-paper-scissors game (user vs computer)

---
+++ rock-paper-scissors/game.py
@@ -0,0 +1,34 @@
+# This program lets the user to play rock-paper-scissors with computer
+
+import random
+
+user_choice = input("Type your choice:\n1. Rock\n2. Paper\n3. Scissors:\n--> ").strip().lower()
+if user_choice == '1':
+    user_choice = "rock"
+elif user_choice == '2':
+    user_choice = "paper"
+elif user_choice == '3':
+    user_choice = "scissors"
+else:
+    print("Invalid choice!")
+    exit()
+
+computer = random.choice(["rock", "paper", "scissors"])
+
+print()
+print(f"You choosed {user_choice.capitalize()}! and Computer choosed {computer.capitalize()}!")
+
+if user_choice == computer:
+    print("Draw!")
+
+elif user_choice == "rock" and computer == "scissors":
+    print("--> User win!")
+
+elif user_choice == "scissors" and computer == "paper":
+    print("--> User win!")
+
+elif user_choice == "paper" and computer == "rock":
+    print("--> User win!")
+
+else:
+    print("Computer wins!")
