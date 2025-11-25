0464522384726cf3dbd790941faa6d4f81a902b5
Ayan Arfeen <ayanarfeenofficial@gmail.com>
2025-11-26 01:29:22 +0500

    Add difficulty selection and attempt tracking to number guessing game

--- guess-the-number/game.py
+++ guess-the-number/game.py
@@ -1,28 +1,60 @@
 # This program lets the user guess the number!
 
 import random
+quit_game = False
 
 while True:
-    secret = random.randint(1, 10)
-
-    print()  # UI spacing
-    user_input = input("Enter your guess number between 1 to 10 or type 'q' to exit: ").strip().lower()
+    print()
+    difficulty = input("Choose difficulty:\n1. Easy (1-10)\n2. Medium (1-50)\n3. Hard (1-100):\n---> ").strip().lower()
     
-    if user_input == 'q':
-        print("Thanks for playing the game.")
-        break
+    if difficulty in ["1", "easy"]:
+        max_range = 10
 
-    try:
-        guess = int(user_input)
-    except ValueError:
-        print("Invalid Input! Please enter a number between 1 to 10.\n")
-        # No play-again question here—just restart cleanly
+    elif difficulty in ["2", "medium"]:
+        max_range = 50
+
+    elif difficulty in ["3", "hard"]:
+        max_range = 100
+
+    else:
+        print("Try Again!!")
         continue
 
-    if guess == secret:
-        print("Correct!")
-    else:
-        print(f"Wrong! The number is {secret}.")
+    secret = random.randint(1, max_range)
+    attempts = 0
+
+    while True:
+        print()  # UI spacing
+        user_input = input("Enter your guess number according to your chosen difficulty or type 'q' to exit: ").strip().lower()
+        
+        if user_input == 'q':
+            print("Thanks for playing the game.")
+            quit_game = True
+            break
+
+        try:
+            guess = int(user_input)
+        except ValueError:
+            print("Please enter a valid number!\n")
+            # No play-again question here—just restart cleanly
+            continue
+
+        attempts += 1
+        
+        if guess < secret:
+            print("Too Low!")
+            continue
+        
+        if guess > secret:
+            print("Too High!")
+            continue
+        
+        if guess == secret:
+            print(f"You guessed it in {attempts} tries!")
+            break
+
+    if quit_game:
+        break
 
     print()   # Better UI spacing
     choice = input("Do you want to play again? (y/n): ").strip().lower()
